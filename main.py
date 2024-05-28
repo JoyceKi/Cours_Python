f2cde8bea52a5bc65266cf696b5de5179146566e
JoyceKi <jocelyne.kim18@gmail.com>
2024-05-28 19:50:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("J'apprends Python!")
+print (17+35*2)
+nom = "James"
+age = 35
+print(f"Je m'appelle {nom} et j'ai {age} ans")
+age = age +10
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant")
+
