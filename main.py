f21a81b65e8ad5974c157d262d4e4e926a5b568e
JoyceKi <jocelyne.kim18@gmail.com>
2024-05-30 14:29:11 +0200

    Types de données et pytest

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 print("J'apprends Python!")
+
 print (17+35*2)
+
 nom = "James"
 age = 35
 print(f"Je m'appelle {nom} et j'ai {age} ans")
+
 age = age +10
 print(f"Je m'appelle {nom} et j'ai {age} ans maintenant")
 
+taille = 1.71
+
+est_etudiant = False
+
+print (f"Nom : {nom}")
+print (f"Age : {age}")
+print (f"Taille : {taille}")
+print (f"Est étudiant : {est_etudiant}")
+
+print (f"Type Nom : {type(nom)}")
+print (f"Type Age : {type(age)}")
+print (f"Type Taille : {type(taille)}")
+print (f"Type Est étudiant : {type(est_etudiant)}")
